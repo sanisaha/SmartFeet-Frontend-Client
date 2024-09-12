00752e2234908152eba3a3229ca66e69bce5ad42
sanisaha <ratulcvasu@gmail.com>
2024-09-12 20:41:07 +0300

    apply react router - create layout - create homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import React from "react";
+import { RouterProvider } from "react-router-dom";
+import router from "./routes/route";
 
 const App = () => {
-  return <div>Hello react</div>;
+  return <RouterProvider router={router}></RouterProvider>;
 };
 
 export default App;
