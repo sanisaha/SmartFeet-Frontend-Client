9831fa608f60fcc6191906f015ac8e4b7c147c71
sanisaha <ratulcvasu@gmail.com>
2024-09-12 20:24:22 +0300

    manage application running - create store for state management

--- src/test/components/appRender.tsx
+++ src/test/components/appRender.tsx
@@ -1,18 +1,15 @@
-import { PropsWithChildren, ReactElement } from "react"
-import { RenderOptions, render } from "@testing-library/react"
-import store from "../shared/store"
-import { Provider } from "react-redux"
+import { PropsWithChildren, ReactElement } from "react";
+import { RenderOptions, render } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { store } from "../../app/data/store";
 
-const appRender = (
-    component: ReactElement,
-    options: RenderOptions = {}
-) => {
-    const Wrapper = ({ children }: PropsWithChildren): JSX.Element => {
-        return <Provider store={store}>{children}</Provider>
-    }
-    return {
-        ...render(component, {wrapper: Wrapper, ...options})
-    }
-}
+const appRender = (component: ReactElement, options: RenderOptions = {}) => {
+  const Wrapper = ({ children }: PropsWithChildren): JSX.Element => {
+    return <Provider store={store}>{children}</Provider>;
+  };
+  return {
+    ...render(component, { wrapper: Wrapper, ...options }),
+  };
+};
 
-export default appRender
+export default appRender;
