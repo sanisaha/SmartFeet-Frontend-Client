931a626914087655e23b600e8e51f83252245c7f
sanisaha <ratulcvasu@gmail.com>
2024-09-13 18:38:28 +0300

    add feature section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Carousel from "../shared/ui/Carousel";
 import CollectionSection from "../shared/ui/CollectionSection";
 import NewArrivals from "../shared/ui/NewArrivals";
+import FeaturedSection from "../shared/ui/FeaturedSection";
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Carousel />
       <NewArrivals />
       <CollectionSection />
+      <FeaturedSection />
     </div>
   );
 };
